80209cb129684b33d3beb449376575211a5d6b0e
MinjiJeon <minji799@gmail.com>
2024-07-16 00:46:22 +0900

    debugging image issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://minji0214.github.io',
-	base: 'minglog.github.io',
-	integrations: [mdx(), sitemap()],
+  site: "https://minji0214.github.io",
+  base: "minglog.github.io",
+  integrations: [mdx(), sitemap(), image()],
 });
