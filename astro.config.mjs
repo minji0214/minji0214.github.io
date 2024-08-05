2a0db4a1b31d8b201a2836547e0a1b3d1cfe0cd1
MinjiJeon <minji799@gmail.com>
2024-08-06 00:52:20 +0900

    svg animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://minji0214.github.io",
   base: "minglog.github.io",
-  integrations: [mdx(), sitemap(), image()],
+  integrations: [mdx(), sitemap(), image(), react()]
 });
