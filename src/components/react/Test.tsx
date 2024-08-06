df6758b0727613b2f0424a3b13116ce810309461
MinjiJeon <minji799@gmail.com>
2024-08-07 00:40:39 +0900

    home

--- src/components/react/Test.tsx
+++ src/components/react/Test.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+// import { Image } from "astro:assets";
+// import bgLogo from "../../assets/bgLogo.png";
 
 export default function Test() {
-  return <div>Test</div>;
+  return (
+    <div>
+      {/* <Image src={myImportedImage} alt="descriptive text" /> */}
+      {/* <h1>Test</h1> */}
+    </div>
+  );
 }
