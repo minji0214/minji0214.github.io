cb53c6f545439d90102426f24dddb76f44233ba1
Minji Jeon <105787441+minji0214@users.noreply.github.com>
2024-10-30 19:39:41 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://minji0214.github.io",
-	base: "minglog",
 	integrations: [
 		mdx(),
-		sitemap({
-			customUrl: (page) => `https://minji0214.github.io/minglog${page}`,
-		}),
 		react(),
 	],
 });
